c0d87d85d3def6b5b533714c1f7fb85fe4e76ba8
Sneha0024 <77921781+Sneha0024@users.noreply.github.com>
2022-10-01 01:25:58 +0530

    Update DoneLogin.js

--- src/components/DoneLogin.js
+++ src/components/DoneLogin.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function DoneLogin (props){
   return (
     <div className="flex h-screen bg-gradient-to-r from-violet-500 to-fuchsia-500 items-center justify-center flex-col">
-      <div className="text-white font-bold text-2xl">{`Hey there ${props.name} !`}</div>
+      <div className="text-white font-bold text-2xl">{`Hey there ${props.name?props.name:""} !`}</div>
       <div className="text-white font-semibold text-xl">You are logged In</div>
       <div> <Link to="/" className="underline text-white font-medium">Back to login</Link></div>
        </div>
